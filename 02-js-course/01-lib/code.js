d7aaf2283f33c96ab729ae4eb9958981955c8636
Gody <250132528+matecg@users.noreply.github.com>
2026-03-10 06:38:16 -0300

    Add function to display books on page

--- 02-js-course/01-lib/code.js
+++ 02-js-course/01-lib/code.js
@@ -15,3 +15,25 @@ function addBookToLibrary(title, author, numOfPages) {
     const newBook = new Book(title, author, numOfPages);
     myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    const container = document.querySelector(".books-container");
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div");
+
+        const titlePara = document.createElement("p");
+        titlePara.textContent = myLibrary[i].title;
+        const infoPara = document.createElement("p");
+        infoPara.textContent = `${myLibrary[i].author} - ${myLibrary[i].numOfPages} pages${myLibrary[i].read ? " - ✅ Done;" : ";"}`;
+
+        bookCard.appendChild(titlePara);
+        bookCard.appendChild(infoPara);
+        container.appendChild(bookCard);
+    }
+}
+
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 293);
+addBookToLibrary("Thinking, fast and slow", "Daniel Kahneman", 497);
+addBookToLibrary("Uncertainty in Games", "Greg Costikyan", 141);
+
+displayBooks();
