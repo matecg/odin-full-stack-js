be96d55249e4aa840bc8497f7b5c8ab452ccd629
Gody <250132528+matecg@users.noreply.github.com>
2026-03-10 09:21:43 -0300

    Add a button to toggle a book's read status

--- 02-js-course/01-lib/code.js
+++ 02-js-course/01-lib/code.js
@@ -13,6 +13,10 @@ function Book(title, author, numOfPages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function addBookToLibrary(title, author, numOfPages, read = false) {
     const newBook = new Book(title, author, numOfPages, read);
     myLibrary.push(newBook);
@@ -25,27 +29,43 @@ function removeBookFromLibrary(id) {
     return myLibrary.splice(idx, 1);
 }
 
+function toggleBookReadStatus(id) {
+    const book = myLibrary.find((book) => book.id === id);
+    book.toggleRead();
+}
+
 function displayBooks() {
     const container = document.querySelector(".books-container");
     // Wipe all children is sub-optimal, better would be a selective display
     container.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
-        const bookCard = document.createElement("div");
-
-        const titlePara = document.createElement("p");
-        titlePara.textContent = myLibrary[i].title;
-        const infoPara = document.createElement("p");
-        infoPara.textContent = `${myLibrary[i].author} - ${myLibrary[i].numOfPages} pages${myLibrary[i].read ? " - ✅ Done;" : ";"}`;
-        const removeBtn = document.createElement("button");
-        removeBtn.textContent = "🗑️";
-        removeBtn.setAttribute("data-book-id", myLibrary[i].id);
-        removeBtn.addEventListener("click", onRemoveBook);
-
-        bookCard.appendChild(titlePara);
-        bookCard.appendChild(infoPara);
-        bookCard.appendChild(removeBtn);
+        const bookCard = createBookCard(myLibrary[i]);
         container.appendChild(bookCard);
     }
+}
+
+function createBookCard(data) {
+    const {id, title, author, numOfPages, read} = data;
+    const bookCard = document.createElement("div");
+
+    const titlePara = document.createElement("p");
+    titlePara.textContent = title;
+    const infoPara = document.createElement("p");
+    infoPara.textContent = `${author} - ${numOfPages} pages${read ? " - ✅ Done;" : ";"}`;
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "🗑️";
+    removeBtn.setAttribute("data-book-id", id);
+    removeBtn.addEventListener("click", onRemoveBook);
+    const readBtn = document.createElement("button");
+    readBtn.textContent = "Mark read";
+    readBtn.setAttribute("data-book-id", id);
+    readBtn.addEventListener("click", onReadBook);
+
+    bookCard.appendChild(titlePara);
+    bookCard.appendChild(infoPara);
+    bookCard.appendChild(removeBtn);
+    bookCard.appendChild(readBtn);
+    return bookCard;
 }
 
 function onSubmitNewBook(e) {
@@ -60,8 +80,14 @@ function onSubmitNewBook(e) {
 }
 
 function onRemoveBook(e) {
-    const {bookId} = e.target.dataset
+    const { bookId } = e.target.dataset
     removeBookFromLibrary(bookId);
+    displayBooks();
+}
+
+function onReadBook(e) {
+    const {bookId} = e.target.dataset;
+    toggleBookReadStatus(bookId);
     displayBooks();
 }
 
