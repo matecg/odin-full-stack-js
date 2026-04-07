c8203a029d4e625decf0e3e46173d199bb7cfd69
Gody <250132528+matecg@users.noreply.github.com>
2026-04-07 06:19:36 -0300

    Add dynamic event checking for multiple test cases

--- 02-js-course/05-form-validation-02/index.js
+++ 02-js-course/05-form-validation-02/index.js
@@ -5,27 +5,71 @@
     const postal = document.querySelector("#postal");
     const pass = document.querySelector("#password");
     const confPass = document.querySelector("pass-confirm");
+    const emailValidations = [
+        {
+            name: "typeMismatch",
+            msg: "Entered values must be an email address."
+        },
+        {
+            name: "valueMissing",
+            msg: "Please provide an email to continue."
+        },
+        {
+            name: "tooShort",
+            msg: "The email provided is too short."
+        }];
 
-    
+    email.addEventListener('input', () => {
+        const source = "email";
+        emailValidations.forEach((validation) => {
+            const { name, msg } = validation;
+            if (email.validity[name]) {
+                if (getErrorMessage(source, name) !== null) return;
+                const errMsg = new ErrorMessage(msg,
+                    source, name
+                );
+                addErrorMessage(errMsg);
+            } else {
+                removeErrorMessage(source, name);
+            }
+        })
+    })
 })()
 
+class ErrorMessage {
+    constructor(msg, source, type) {
+        this.msg = msg;
+        this.source = source;
+        this.type = type;
+    }
+}
+
 function addErrorMessage(msgData) {
-    const {id, msg} = msgData;
+    const { msg, source, type } = msgData;
     const errorsList = document.querySelector(".errors");
     const errorItem = document.createElement('li');
     errorItem.textContent = msg;
-    errorItem.dataset.id = id;
+    errorItem.dataset.source = source;
+    errorItem.dataset.type = type;
     errorItem.classList.add('error');
     errorsList.appendChild(errorItem);
 }
 
-function removeErrorMessage(id) {
+function removeErrorMessage(source, type) {
     const errorsList = document.querySelector(".errors");
-    let children = [...errorsList.childNodes];
-    const toRemove = children.find(child => {
+    const toRemove = getErrorMessage(source, type);
+    if (!toRemove) return;
+    errorsList.removeChild(toRemove);
+}
+
+function getErrorMessage(source, type) {
+    const errorList = document.querySelector(".errors");
+    let children = [...errorList.children];
+    let child = children.find((child) => {
         if (child.dataset)
-            return child.dataset.id === `${id}`;
+            return child.dataset.source === source && child.dataset.type === type;
         return false;
     });
-    errorsList.removeChild(toRemove);
+    child = child === undefined ? null : child;
+    return child;
 }
